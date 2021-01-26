82a64a1119afd539ca9d75f6a97a4c6dc3c6509e
Chris <crhawkins85@gmail.com>
2021-01-26 20:38:27 +0000

    Updating to new syntax

--- src/ApiB/Startup.cs
+++ src/ApiB/Startup.cs
@@ -29,8 +29,7 @@ namespace ApiB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
+            services.AddMicrosoftIdentityWebApiAuthentication(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
