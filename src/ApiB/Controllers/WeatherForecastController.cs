455de2585a42b8ac14925b69a42f270f3e12923c
Chris Hawkins <chris@chrishawkins.org>
2021-01-27 21:59:24 +0000

    Sonar tidyup

--- src/ApiB/Controllers/WeatherForecastController.cs
+++ src/ApiB/Controllers/WeatherForecastController.cs
@@ -18,16 +18,6 @@ namespace ApiB.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
-        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
